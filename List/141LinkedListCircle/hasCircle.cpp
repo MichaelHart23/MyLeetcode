53bc9c79c8a07687b5ae573dd49c0f6709afb553
MichaelHart <handongxiu23@gmail.com>
2026-05-10 15:04:44 +0800

    20260510

--- List/141LinkedListCircle/hasCircle.cpp
+++ List/141LinkedListCircle/hasCircle.cpp
@@ -1,11 +1,14 @@
 
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next) {
+    }
 };
 
 /**
@@ -13,13 +16,13 @@ struct ListNode {
  */
 class Solution {
 public:
-  bool hasCycle(ListNode *head) {
-    ListNode* fast = head, *slow = head;
-    while(fast && fast->next) {
-      fast = fast->next->next;
-      slow = slow->next;
-      if(fast == slow) return true;
+    bool hasCycle(ListNode* head) {
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
     }
-    return false;
-  }
 };
--- List/19RemoveNthFromEnd/removeNthFromEnd.cpp
+++ List/19RemoveNthFromEnd/removeNthFromEnd.cpp
@@ -1,27 +1,68 @@
 
 struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {
+    }
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next) {
+    }
 };
 
 class Solution {
 public:
-  ListNode* removeNthFromEnd(ListNode* head, int n) {
-    ListNode dummyHead = ListNode(0, head);
-    ListNode* left = &dummyHead, *right = &dummyHead;
-    while(n--) {
-      right = right->next;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummyHead = ListNode(0, head);
+        ListNode *left = &dummyHead, *right = &dummyHead;
+        while (n--) {
+            right = right->next;
+        }
+        while (right->next) {
+            right = right->next;
+            left = left->next;
+        }
+        ListNode* del = left->next;
+        left->next = del->next;
+        delete del;
+        return dummyHead.next;
     }
-    while(right->next) {
-      right = right->next;
-      left = left->next;
+};
+
+// 没有考虑删除头节点的情况
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* front = head, *back = head;
+        while(n > 0 && front != nullptr) {
+            n--;
+            front = front->next;
+        }
+        while(front->next != nullptr) {
+            front = front->next;
+            back = back->next;
+        }
+        back->next = back->next->next;
+        return head;
+
     }
-    ListNode* del = left->next;
-    left->next = del->next;
-    delete del;
-    return dummyHead.next;
-  }
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummyHead(0, head);
+        ListNode* front = &dummyHead, *back = &dummyHead;
+        while(n > 0 && front != nullptr) {
+            n--;
+            front = front->next;
+        }
+        while(front->next != nullptr) {
+            front = front->next;
+            back = back->next;
+        }
+        back->next = back->next->next;
+        return dummyHead.next;
+
+    }
 };
